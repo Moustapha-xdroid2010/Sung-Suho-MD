ee27d2cbea0182face2a076be6eff4e4e94c79cf
Moustapha-xdroid2010 <moustaphax2010@gmail.com>
2025-06-06 11:27:08 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "suho~cicDDTSS#i8T7nBejAWvgUcRI-orFjahE51vzmwuLqkQkNVKIww4",
     // add your Session Id make sure it starts with suho~
 
     PREFIX: process.env.PREFIX || ".",
